b86df42da154488a6c30ab9dcbc9dc9bea4e5592
Dustin Hamano <dhamano@Dustins-iMac-Pro.local>
2019-07-17 14:24:55 -0700

    MVP and some stretch done

---
+++ onboarding/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import FormikLoginForm from './components/RegisterForm';
+
+function App() {
+  return (
+    <div className="App">
+      <FormikLoginForm />
+    </div>
+  );
+}
+
+export default App;
